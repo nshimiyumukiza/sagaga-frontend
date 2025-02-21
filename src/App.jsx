2c765d8494d62dbeaab86e2b72eb835c8400136b
nshimiyumukiza <nshimiyumukizaerneste99@gmail.com>
2025-02-21 09:10:40 +0200

    add register form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter,Routes,Route } from "react-router"
-import Navbar from "./components/Navbar"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import Layout from "./pages/Layout"
+import Home from "./pages/Home"
 
 const App = () => {
   return (
@@ -10,12 +10,12 @@ const App = () => {
 <BrowserRouter>
 <Routes>
   <Route element={<Layout />}>
+  <Route path="/"element={<Home />}/>
   <Route path="/login"element={<Login />}/>
   <Route path="/register"element={<Register />}/>
   </Route>
 </Routes>
 </BrowserRouter>
-
     </div>
   )
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,32 +1,75 @@
-import React from 'react'
-import { Link } from 'react-router'
+import { useState } from "react";
+import { Link } from "react-router";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
   return (
-    <div className='bg-gray-100 flex justify-center'>
-        <div>
-           <form className='space-y-4 bg-gray-200 mb-4 px-3' action="submit">
-            <label className="flex flex-col">
-                <span>Name</span>
-                <input className='border rounded-md outline-none px-3 py-1.5 bg-gray-50' type="text"placeholder='Enter Name' />
-            </label>
-            <label className="flex flex-col">
-                <span>Email</span>
-                <input className='border rounded-md outline-none px-3 py-1.5 bg-gray-50' type="text"placeholder='Enter Email' />
-            </label>
-            <label className="flex flex-col">
-                <span>Password</span>
-                <input className='border rounded-md outline-none px-3 py-1.5 bg-gray-50' type="text"placeholder='Ent Password' />
-            </label>
-            <div className='flex justify-center'>
-            <Link className='w-full text-center text-blue-600 px-2 py-1 bg-green-400 hover:text-white'>Submit</Link>
-            </div>
-            
-           </form>
-        </div>
-     <h1></h1> 
+    <div className="bg-gray-100 flex justify-center">
+      <div>
+        <form
+          onSubmit={handleSubmit}
+          className="space-y-4 bg-gray-200 mb-4 px-3"
+          action="submit"
+        >
+          <label className="flex flex-col">
+            <span>Name</span>
+            <input
+              className="border rounded-md outline-none px-3 py-1.5 bg-gray-50"
+              type="text"
+              name="name"
+              onChange={handleChange}
+              value={formData.name}
+              placeholder="Enter Name"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span>Email</span>
+            <input
+              className="border rounded-md outline-none px-3 py-1.5 bg-gray-50"
+              type="text"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="Enter Email"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span>Password</span>
+            <input
+              className="border rounded-md outline-none px-3 py-1.5 bg-gray-50"
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder="Enter Password"
+            />
+          </label>
+          <div className="flex justify-center">
+            <Link
+              type="submit"
+              onClick={handleSubmit}
+              className="w-full text-center text-blue-600 px-2 py-1 bg-green-400 hover:text-white"
+            >
+              Submit
+            </Link>
+          </div>
+        </form>
+      </div>
+      <h1></h1>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
